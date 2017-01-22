77f1c62b71151cba53caaa7a874e9ca369e22c6d
Jens Kehne <jens@int80.de>
2017-01-22 14:25:09 +0100

    Add game IDs to detect dropped connections

--- server.py
+++ server.py
@@ -2,25 +2,58 @@
 
 from websocket_server import WebsocketServer
 
+current_game_id = 0
+last_winner = ""
+
 # Called for every client connecting (after handshake)
 def new_client(client, server):
+        global current_game_id
         print("New client connected and was given id %d" % client['id'])
+        server.send_message(client, "SIGNIN;%u;%s" % (current_game_id, last_winner))
 
 # Called for every client disconnecting
 def client_left(client, server):
         print("Client(%d) disconnected" % client['id'])
 
-# Called when a client sends a message
-def message_received(client, server, message):
-        if message == 'PING':
-                print "ping received from client %d" % (client['id'])
-                server.send_message(client, "PONG")
+def handle_ping(client, server):
+        print "ping received from client %d" % (client['id'])
+        server.send_message(client, "PONG")
+        return
+
+def handle_win(client, server, params):
+        global current_game_id, last_winner
+        gameid, winner = params.split(";", 1)
+
+        if int(gameid) != current_game_id:
+                print "Invalid game id %u (should be %u)" % (int(gameid), current_game_id)
                 return
+
+        print "%s wins game %u" % (winner, current_game_id)
+
+        current_game_id += 1
+        last_winner = winner
+
         for cli in server.clients:
                 if cli == client:
                         continue
-                server.send_message(cli, message.encode('utf-8'))
+                server.send_message(cli, "WIN;%u;%s" % (current_game_id, winner.encode('utf-8')))
 
+def handle_unknown_opcode(message):
+        print "Opcode not recognized. Message was: %s" % message
+
+# Called when a client sends a message
+def message_received(client, server, message):
+        if message == 'PING':
+                handle_ping(client, server)
+
+        opcode, params = message.split(";", 1)
+        if opcode == "WIN":
+                handle_win(client, server, params)
+        else:
+                handle_unknown_opcode(message)
+
+
+# def main():
 PORT=9001
 server = WebsocketServer(PORT)
 server.set_fn_new_client(new_client)
